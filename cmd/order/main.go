fe526b2fd4c329e7507597f40e305231d959fe99
ElanUtta <elanlima19@gmail.com>
2023-11-30 21:43:47 -0300

    rabbitmq config and main order api

--- cmd/order/main.go
+++ cmd/order/main.go
@@ -4,9 +4,8 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"strconv"
-	"time"
 
+	"github.com/elanutta/go-intensivo/internal/infra/database"
 	"github.com/elanutta/go-intensivo/internal/usecase"
 	"github.com/elanutta/go-intensivo/pkg/rabbitmq"
 	_ "github.com/mattn/go-sqlite3"
@@ -19,52 +18,20 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	defer db.Close()
 
-	// orderRepository := database.NewOrderRepository(db)
+	orderRepository := database.NewOrderRepository(db)
 
-	// uc := usecase.NewCalculateFinalPrice(orderRepository)
+	uc := usecase.NewCalculateFinalPrice(orderRepository)
 	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
 		panic(err)
 	}
-
 	defer ch.Close()
 
-	// msgRabbitmqChannel := make(chan amqp.Delivery)
-
-	canal := make(chan usecase.OrderInput)
-
-	go func() {
-		for i := 4; i < 1000000; i++ {
-			my_str := strconv.Itoa(i)
-			canal <- usecase.OrderInput{my_str, 3.0, 3.1}
-		}
-	}()
-
-	// for n := range canal {
-	// 	fmt.Println(n)
-	// }
-
-	// time.Sleep(time.Second * 10)
-
-	// println(canal)
-
-	start := time.Now()
-
-	for n := range canal {
-		go rabbitmq.Publisher(ch, n)
-		go rabbitmq.Publisher(ch, n)
-		go rabbitmq.Publisher(ch, n)
-		rabbitmq.Publisher(ch, n)
-		// rabbitmq.Publisher(ch, n)
-	}
-
-	println("time duration", time.Since(start))
-
-	// go rabbitmq.Consume(ch, msgRabbitmqChannel)
-	// rabbitmqWorker(msgRabbitmqChannel, uc)
+	msgRabbitmqChannel := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, msgRabbitmqChannel)
+	rabbitmqWorker(msgRabbitmqChannel, uc)
 
 }
 
--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -1,11 +1,6 @@
 package rabbitmq
 
 import (
-	"context"
-	"encoding/json"
-	"time"
-
-	"github.com/elanutta/go-intensivo/internal/usecase"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -41,75 +36,4 @@ func Consume(ch *amqp.Channel, out chan amqp.Delivery) error {
 		out <- msg
 	}
 	return nil
-}
-
-func Publisher(ch *amqp.Channel, text_msg usecase.OrderInput) error {
-
-	err := ch.ExchangeDeclare(
-		"oerder_exchange", // name
-		"fanout",          // type
-		true,              // durable
-		false,             // auto-deleted
-		false,             // internal
-		false,             // no-wait
-		nil,               // arguments
-	)
-
-	if err != nil {
-		return err
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	json_message, err := json.Marshal(text_msg)
-	if err != nil {
-		return err
-	}
-
-	err = ch.PublishWithContext(ctx,
-		"oerder_exchange", // exchange
-		"nao_sei",         // routing key
-		false,             // mandatory
-		false,             // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(json_message),
-		})
-
-	ch.QueueBind(
-		"order",
-		"nao_sei",
-		"oerder_exchange",
-		false,
-		nil,
-	)
-
-	return nil
-
-	// msg := amqp.Publishing{
-	// 	DeliveryMode: amqp.Persistent,
-	// 	Timestamp:    time.Now(),
-	// 	ContentType:  "application/json",
-	// 	Body:         []byte(json_message),
-	// }
-
-	// err = ch.PublishWithContext(
-	// 	ctx,
-	// 	"order",
-	// 	"go-delivery",
-	// 	false,
-	// 	false,
-	// 	msg,
-	// )
-	// println("foi publicado a messagem ", json_message)
-
-	// if err != nil {
-	// 	// Since publish is asynchronous this can happen if the network connection
-	// 	// is reset or if the server has run out of resources.
-	// 	log.Fatalf("basic.publish: %v", err)
-	// }
-
-	// return nil
-
 }
